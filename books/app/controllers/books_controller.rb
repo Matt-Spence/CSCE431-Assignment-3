11ff8ee6ea62b83dbf28f0790d650198ef7f5cf4
Matt-Spence <matthewswspence@gmail.com>
2020-09-15 11:51:38 -0500

    Finished full initial implementation

--- books/app/controllers/books_controller.rb
+++ books/app/controllers/books_controller.rb
@@ -8,16 +8,41 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])  
   end
 
   def create
     @book = Book.new(book_params)
-
-    @book.save
-    redirect_to @book
+	if @book.save
+	  redirect_to @book
+	else
+	  render 'new'
+	end
   end
+
+  def update
+    @book = Book.find(params[:id])  
+    
+    if @book.update(book_params)
+	redirect_to @book
+    else
+	render 'edit'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])  
+    @book.destroy
+    redirect_to books_path
+  end
+
   private
     def book_params
       params.require(:book).permit(:title, :author, :genre, :date, :price, :published_date)
     end
+
 end
--- books/app/models/book.rb
+++ books/app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 4}
+  validates :author, presence: true, length: {minimum: 4}
+  validates :genre, presence: true
+  validates :price, presence: true, length: {minimum: 1}
+  validates :published_date, presence: true
 end
