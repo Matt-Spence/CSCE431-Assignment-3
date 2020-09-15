44be9f230e957087ddb6e3e4873307ce8381f175
Matt-Spence <matthewswspence@gmail.com>
2020-09-15 10:57:10 -0500

    Finished show, new, and index routes

--- books/app/controllers/books_controller.rb
+++ books/app/controllers/books_controller.rb
@@ -1,8 +1,23 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:book].inspect
+    @book = Book.new(book_params)
+
+    @book.save
+    redirect_to @book
   end
+  private
+    def book_params
+      params.require(:book).permit(:title, :author, :genre, :date, :price, :published_date)
+    end
 end
